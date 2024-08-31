d8bd90f5aede4ac40f22c25dcfe32aab85c827c1
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-09-01 01:18:57 +0200

    Updated HomeView

--- MarsRover/Views/HomeView.swift
+++ MarsRover/Views/HomeView.swift
@@ -15,16 +15,34 @@ struct HomeView: View {
             VStack{
                 //header
                 HeaderView()
-                
+                if !viewModel.photos.isEmpty {
+                    ScrollView{ // TODO()add top padding 20
+                        VStack(spacing: 12){
+                            ForEach(viewModel.photos) { photo in
+                                DetailCard(photo: photo)
+                            }
+                        }
+                    }
+                } else {
+                    if #available(iOS 14.0, *) {
+                        ProgressView("Data is loading...")
+                            .padding(.top, 200)
+                    } else {
+                        Text("Data is loading...")
+                            .font(.system(size: 17, weight: .bold))
+                            .padding(.top, 200)
+                    }
+                }
                 Spacer()
             }
+            .background(Color.BackgroundOne)
             .edgesIgnoringSafeArea(.all)
         }
     }
 }
 
 #Preview {
-    HomeView()
+    HomeView(viewModel: RoverViewModel())
 }
 
 
@@ -38,3 +56,27 @@ extension Color {
 }
 
 
+
+//if #available(iOS 15.0, *) {
+//                                        AsyncImage(
+//                                            url: URL(string: photo.imgSrc)) { image in
+//                                                image
+//                                                    .resizable()
+//                                                    .scaledToFit()
+//                                                    .frame(width: 130, height: 130)
+//                                                    .clipShape(.rect(cornerRadius: 20))
+//                                                    .padding(10)
+//                                            } placeholder: {
+//                                                Rectangle()
+//                                                    .fill(Color.gray.opacity(0.2))
+//                                                    .frame(width: 130, height: 130)
+//                                                    .clipShape(RoundedRectangle(cornerRadius: 20))
+//                                                    .padding(10)
+//                                            }
+//                                    } else {
+//                                        Rectangle() // TODO() make it works with ios 13
+//                                            .fill(Color.red)
+//                                            .frame(width: 130, height: 130)
+//                                            .clipShape(.rect(cornerRadius: 20))
+//                                            .padding(10)
+//                                    }

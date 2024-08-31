2b0e94ebc737da3a06c144773ef42be25c134f5d
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-08-31 16:48:21 +0200

    Implemented HomeView
    with Custom Colours

---
+++ MarsRover/Views/Components/FilterButton.swift
@@ -0,0 +1,18 @@
+//
+//  FilterButton.swift
+//  MarsRover
+//
+//  Created by Vladyslav Dikhtiaruk on 31/08/2024.
+//
+
+import SwiftUI
+
+struct FilterButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterButton()
+}
---
+++ MarsRover/Views/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  MarsRover
+//
+//  Created by Vladyslav Dikhtiaruk on 31/08/2024.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}

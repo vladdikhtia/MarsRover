80c24e489f83d257a19a5742abee2f6e13c6e0da
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-08-31 16:50:15 +0200

    Implemented HeaderView

--- MarsRover/Views/HeaderView.swift
+++ MarsRover/Views/HeaderView.swift
@@ -9,7 +9,48 @@ import SwiftUI
 
 struct HeaderView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            VStack(alignment: .leading){
+                Text("MARS.CAMERA") // finish fonts
+                    .font(.system(size: 34, weight: .bold))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Text("June 6, 2019")
+                    .font(.system(size: 17, weight: .bold))
+                
+                Spacer()
+                
+                HStack(spacing: 12){
+                    FilterButton(imageName: "rover", title: "All", action: {print("Pressed Filter Rover")})
+                    
+                    FilterButton(imageName: "camera", title: "All", action: {print("Pressed Filter Camera")})
+                }
+            }
+            
+            Spacer()
+            
+            VStack(alignment: .center){
+                Button(action: {
+                    print("Calendar pressed")
+                }, label: {
+                    Image("calendar")
+                        .resizable()
+                        .frame(width: 30, height: 34)
+                })
+                .padding(.top, 12)
+                .frame(width: 44, height: 44, alignment: .center)
+                
+                Spacer()
+                
+                FilterButton(imageName: "addMy", title: "", action: {print("Pressed Save Button")})
+            }
+        }
+        .foregroundColor(Color.LayerOne)
+        .padding(.top, 54) // remove if Iphone with TouchID
+        .padding(.vertical)
+        .padding(.horizontal, 20)
+        .background(Color.accentOne)
+        .frame(height: 202)
     }
 }
 

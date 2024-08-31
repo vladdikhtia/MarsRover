92ab3b28cdb616776de4141cb3cf0973efa223da
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-08-31 16:49:55 +0200

    Implemented HomeView
    with Custom Colours (note: Wrong previous commit wrong naming)

---
+++ MarsRover/Views/HomeView.swift
@@ -0,0 +1,39 @@
+//
+//  HomeView.swift
+//  MarsRover
+//
+//  Created by Vladyslav Dikhtiaruk on 31/08/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        
+        NavigationView {
+            VStack{
+                //header
+                HeaderView()
+                
+                Spacer()
+            }
+            .edgesIgnoringSafeArea(.all)
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
+
+
+extension Color {
+    static let BackgroundOne = Color("BackgroundOne")
+    static let AccentOne = Color("AccentOne")
+    static let LayerOne = Color("LayerOne")
+    static let LayerTwo = Color("LayerTwo")
+    static let SystemTwo = Color("SystemTwo")
+    static let SystemThree = Color("SystemThree")
+}
+
+

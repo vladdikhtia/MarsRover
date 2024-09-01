531ea0deef024cf6c20835d1c9f6820707304b25
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-09-02 00:32:16 +0200

    Updated Icons

---
+++ MarsRover/Views/Components/CustomDatePicker.swift
@@ -0,0 +1,18 @@
+//
+//  CustomDatePicker.swift
+//  MarsRover
+//
+//  Created by Vladyslav Dikhtiaruk on 01/09/2024.
+//
+
+import SwiftUI
+
+struct CustomDatePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomDatePicker()
+}
---
+++ MarsRover/Views/Components/FilterSheet.swift
@@ -0,0 +1,18 @@
+//
+//  FilterSheet.swift
+//  MarsRover
+//
+//  Created by Vladyslav Dikhtiaruk on 01/09/2024.
+//
+
+import SwiftUI
+
+struct FilterSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterSheet()
+}

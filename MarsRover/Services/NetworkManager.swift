dc5f450bfa308e7f7cf314a9a99275ece2ae2765
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-09-02 00:35:38 +0200

    Updated NetworkManager
    With queryItems

--- MarsRover/Services/NetworkManager.swift
+++ MarsRover/Services/NetworkManager.swift
@@ -7,11 +7,12 @@
 
 import Foundation
 import Combine
+//use this for camera: https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&camera=fhaz&&api_key=DEMO_KEY
+// camera with pagination: https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&camera=fhaz&page=1&api_key=DEMO_KEY
+
 // http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01004/opgs/edr/fcam/FLB_486615455EDR_F0481570FHAZ00323M_.JPG
 // http://mars.nasa.gov/mer/gallery/all/1/p/4037/1P486568465EFFCNJDP2407L2M1-BR.JPG
 
-//    https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=DEMO_KEY - old
-//  with paging https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?earth_date=2015-6-3&page=2&api_key=DEMO_KEY
 class NetworkManager {
     @Published var returnedPhotos: [Photo] = []
     var cancellables = Set<AnyCancellable>()
@@ -26,9 +27,44 @@ class NetworkManager {
         guard let url = URL(string: "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&page=1&api_key=DEMO_KEY") else {
             print("DEBUG: Invalid URL")
             return
+    
+    private func createURLComponents(rover: String, date: String, camera: MarsCamera, page: Int) -> URLComponents {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "api.nasa.gov"
+        urlComponents.path = "/mars-photos/api/v1/rovers/\(rover)/photos"
+        
+//        urlComponents.queryItems = [
+//            URLQueryItem(name: "earth_date", value: date),
+////            URLQueryItem(name: "camera", value: camera),
+//            URLQueryItem(name: "page", value: "\(page)"),
+//            URLQueryItem(name: "api_key", value: apiKey)
+//        ]
+        var queryItems = [
+                URLQueryItem(name: "earth_date", value: date),
+                URLQueryItem(name: "page", value: "\(page)"),
+                URLQueryItem(name: "api_key", value: apiKey)
+        ]
+        
+        if camera != .all {
+            queryItems.append(URLQueryItem(name: "camera", value: camera.rawValue))
         }
-                
-        URLSession.shared.dataTaskPublisher(for: url)
+        
+        urlComponents.queryItems = queryItems
+        
+        return urlComponents
+    }
+    
+    func getData(rover: String, date: String, camera: MarsCamera, page: Int) throws {
+        guard let url = createURLComponents(rover: rover, date: date, camera: camera, page: page).url else {
+            print("DEBUG: Bad URL")
+            throw URLError(.badURL)
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTaskPublisher(for: request)
             .subscribe(on: DispatchQueue.global(qos: .background)) //dataTaskPublisher automatically do it on background
             .receive(on: DispatchQueue.main)
             .tryMap (validatingOutput)
@@ -56,5 +92,53 @@ class NetworkManager {
             throw URLError(.fileDoesNotExist)
         }
         return output.data
+    }
+}
+
+enum MarsRover: String, CaseIterable {
+    case curiosity = "Curiosity"
+    case opportunity = "Opportunity"
+    case spirit = "Spirit"
+    
+    var path: String {
+        return self.rawValue.lowercased()
+    }
+}
+
+enum MarsCamera: String, CaseIterable {
+    case all = "ALL"
+    case fhaz = "FHAZ"
+    case rhaz = "RHAZ"
+    case mast = "MAST"
+    case chemcam = "CHEMCAM"
+    case mahli = "MAHLI"
+    case mardi = "MARDI"
+    case navcam = "NAVCAM"
+    case pancam = "PANCAM"
+    case minites = "MINITES"
+    
+    var description: String {
+        switch self {
+        case .all:
+            return "All Cameras"
+        case .fhaz:
+            return "Front Hazard Avoidance Camera"
+        case .rhaz:
+            return "Rear Hazard Avoidance Camera"
+        case .mast:
+            return "Mast Camera"
+        case .chemcam:
+            return "Chemistry and Camera Complex"
+        case .mahli:
+            return "Mars Hand Lens Imager"
+        case .mardi:
+            return "Mars Descent Imager"
+        case .navcam:
+            return "Navigation Camera"
+        case .pancam:
+            return "Panoramic Camera"
+        case .minites:
+            return "Miniature Thermal Emission Spectrometer (Mini-TES)"
+        }
     }
 }

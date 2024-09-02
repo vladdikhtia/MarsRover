a46752187fcf80d0b8fa9620457f6f4922bd89bd
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-09-02 23:31:52 +0200

    Implemented ImageCacheHelper
    FilterEntity model

--- MarsRover/Services/ImageCacheHelper.swift
+++ MarsRover/Services/ImageCacheHelper.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import SwiftUI
+
+class ImageCacheHelper {
+    static let instance = ImageCacheHelper()
+    
+    private init() {}
+    
+    private let cache = NSCache<NSString, UIImage>()
+    
+    func getImage(key: String) -> UIImage? {
+        cache.object(forKey: key as NSString)
+    }
+    
+    func saveImage(image: UIImage, key: String) {
+        cache.setObject(image, forKey: key as NSString)
+    }
+}
--- MarsRover/Utilities/Persistence.swift
+++ MarsRover/Utilities/Persistence.swift
@@ -10,26 +10,30 @@ import CoreData
 struct PersistenceController {
     static let shared = PersistenceController()
 
-    static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-        }
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
-        return result
-    }()
+//    static var preview: PersistenceController = {
+//        let result = PersistenceController(inMemory: true)
+//        let viewContext = result.container.viewContext
+//        for _ in 0..<10 {
+//            let newItem = Item(context: viewContext)
+//            newItem.timestamp = Date()
+//        }
+//        do {
+//            try viewContext.save()
+//        } catch {
+//            // Replace this implementation with code to handle the error appropriately.
+//            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//            let nsError = error as NSError
+//            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//        }
+//        return result
+//    }()
 
     let container: NSPersistentContainer
 
+    var viewContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
     init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "MarsRover")
         if inMemory {
@@ -49,6 +53,8 @@ struct PersistenceController {
                  Check the error message to determine what the actual problem was.
                  */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
+            } else {
+                print("Successfully loaded core data!")
             }
         })
         container.viewContext.automaticallyMergesChangesFromParent = true

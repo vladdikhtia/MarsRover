d599acffed8df1efaa3de6ed61d09869a095b2af
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-09-04 00:15:21 +0200

    Updated HistoryView
    with ActionSheet

--- MarsRover/Views/HistoryView.swift
+++ MarsRover/Views/HistoryView.swift
@@ -8,9 +8,12 @@
 import SwiftUI
 
 struct HistoryView: View {
-    @ObservedObject var viewModel: CoreDataViewModel
+    @ObservedObject var roverViewModel: RoverViewModel
+    @ObservedObject var coreDataViewModel: CoreDataViewModel
     @Environment(\.presentationMode) var presentationMode
-
+    @State private var isActionSheetPresented = false
+    @State private var selectedFilter: FilterEntity? = nil
+    
     var body: some View {
         VStack(spacing: 0) {
             //header
@@ -25,7 +28,7 @@ struct HistoryView: View {
                 
                 Text("History")
                     .font(.system(size: 34, weight: .bold))
-                    .foregroundColor(Color.layerOne)
+                    .foregroundColor(Color.LayerOne)
                     .frame(maxWidth: .infinity, alignment: .center)
             }
             .padding(.top, 54)
@@ -37,22 +40,26 @@ struct HistoryView: View {
                 y: 5
             )
             
-            if !viewModel.filters.isEmpty {
+            if !coreDataViewModel.filters.isEmpty {
                 ScrollView {
                     VStack(spacing: 12) {
-                        ForEach(viewModel.filters) { filter in
+                        ForEach(coreDataViewModel.filters) { filter in
                             HistoryCard(filter: filter)
                                 .padding(.horizontal, 20)
+                                .onTapGesture {
+                                    self.selectedFilter = filter
+                                    self.isActionSheetPresented = true
+                                }
                         }
                     }
                     .padding(.top, 20)
                 }
             } else {
                 Spacer()
-
+                
                 Image("groupIm")
                     .opacity(0.8)
-                    
+                
                 Text("Browsing history is empty.")
                     .font(.system(size: 16, weight: .regular))
                     .foregroundColor(Color.LayerTwo)
@@ -61,11 +68,38 @@ struct HistoryView: View {
                 Spacer()
             }
         }
+        .actionSheet(isPresented: $isActionSheetPresented) {
+            ActionSheet(
+                title: Text("Menu Filter"),
+                buttons: [
+                    .default(Text("Use"), action: {
+                        guard let filter = selectedFilter else {
+                            print("ERROR")
+                            return
+                        }
+                        roverViewModel.currentRover = MarsRover(rawValue: filter.rover ?? "Curiosity") ?? .curiosity
+                        roverViewModel.currentCamera = MarsCamera(rawValue: filter.camera ?? "PANCAM") ?? .pancam
+                        roverViewModel.currentDate = filter.date ?? "2006-9-10"
+                        self.presentationMode.wrappedValue.dismiss()
+                        roverViewModel.fetchData()
+                        
+                    }),
+                    .destructive(Text("Delete"), action: {
+                        withAnimation {
+                            if let filter = selectedFilter {
+                                coreDataViewModel.deleteFilter(filter: filter)
+                            }
+                        }
+                    }),
+                    .cancel(Text("Cancel"))
+                ]
+            )
+        }
         .edgesIgnoringSafeArea(.all)
-        
-    }        
+    }
 }
 
 #Preview {
-    HistoryView(viewModel: CoreDataViewModel())
+    HistoryView(roverViewModel: RoverViewModel(), coreDataViewModel: CoreDataViewModel())
 }
+

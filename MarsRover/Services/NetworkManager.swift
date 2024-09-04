daa59c5337ff53fd4bdf9bcd2e650a244e5e6fd1
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-09-04 17:20:48 +0200

    Added isLoading to NetworkManager

--- MarsRover/Services/NetworkManager.swift
+++ MarsRover/Services/NetworkManager.swift
@@ -7,20 +7,16 @@
 
 import Foundation
 import Combine
-//use this for camera: https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&camera=fhaz&&api_key=DEMO_KEY
-// camera with pagination: https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&camera=fhaz&page=1&api_key=DEMO_KEY
-
-// http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01004/opgs/edr/fcam/FLB_486615455EDR_F0481570FHAZ00323M_.JPG
-// http://mars.nasa.gov/mer/gallery/all/1/p/4037/1P486568465EFFCNJDP2407L2M1-BR.JPG
 
 class NetworkManager {
     @Published var returnedPhotos: [Photo] = []
     var cancellables = Set<AnyCancellable>()
+    var isLoading: Bool = false
     
-    static let instance = NetworkManager() // singleton
+    static let instance = NetworkManager()
+    private init(){}
     
-    private init(){
-    }
+    private let apiKey: String = "rlXu2zzTUOLFHRWP9RtLE73eqmwPrhzj8RKmeyBa"
     
     private func createURLComponents(rover: String, date: String, camera: MarsCamera, page: Int) -> URLComponents {
         var urlComponents = URLComponents()
@@ -29,25 +25,28 @@ class NetworkManager {
         urlComponents.path = "/mars-photos/api/v1/rovers/\(rover)/photos"
         
         var queryItems = [
-                URLQueryItem(name: "earth_date", value: date),
-                URLQueryItem(name: "page", value: "\(page)"),
-                URLQueryItem(name: "api_key", value: apiKey)
+            URLQueryItem(name: "earth_date", value: date),
+            URLQueryItem(name: "page", value: "\(page)"),
+            URLQueryItem(name: "api_key", value: apiKey)
         ]
         
         if camera != .all {
             queryItems.append(URLQueryItem(name: "camera", value: camera.rawValue))
         }
-        
         urlComponents.queryItems = queryItems
-        
         return urlComponents
     }
     
     func getData(rover: String, date: String, camera: MarsCamera, page: Int) throws {
+        self.isLoading = true
+        print("IsLoading in the begining of getData: \(isLoading)")
         guard let url = createURLComponents(rover: rover, date: date, camera: camera, page: page).url else {
             print("DEBUG: Bad URL")
+            self.isLoading = false
             throw URLError(.badURL)
         }
+        print("IsLoading in the middle of getData: \(isLoading)")
+        
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
@@ -65,8 +64,10 @@ class NetworkManager {
                     print("Error during downloading! \(error.localizedDescription)")
                 }
             } receiveValue: { [weak self] receivedData in
-                print("Raw JSON Data: \(receivedData.photos)")
+                print("Raw JSON Data: \(receivedData.photos.count)")
                 self?.returnedPhotos = receivedData.photos
+                self?.isLoading = false
+                print("IsLoading in the completion of getData: \(String(describing: self?.isLoading))")
             }
             .store(in: &cancellables)
     }
@@ -81,51 +82,36 @@ class NetworkManager {
         return output.data
     }
 }
-
-enum MarsRover: String, CaseIterable {
-    case curiosity = "Curiosity"
-    case opportunity = "Opportunity"
-    case spirit = "Spirit"
-    
-    var path: String {
-        return self.rawValue.lowercased()
-    }
-}
-
-enum MarsCamera: String, CaseIterable {
-    case all = "ALL"
-    case fhaz = "FHAZ"
-    case rhaz = "RHAZ"
-    case mast = "MAST"
-    case chemcam = "CHEMCAM"
-    case mahli = "MAHLI"
-    case mardi = "MARDI"
-    case navcam = "NAVCAM"
-    case pancam = "PANCAM"
-    case minites = "MINITES"
-    
-    var description: String {
-        switch self {
-        case .all:
-            return "All Cameras"
-        case .fhaz:
-            return "Front Hazard Avoidance Camera"
-        case .rhaz:
-            return "Rear Hazard Avoidance Camera"
-        case .mast:
-            return "Mast Camera"
-        case .chemcam:
-            return "Chemistry and Camera Complex"
-        case .mahli:
-            return "Mars Hand Lens Imager"
-        case .mardi:
-            return "Mars Descent Imager"
-        case .navcam:
-            return "Navigation Camera"
-        case .pancam:
-            return "Panoramic Camera"
-        case .minites:
-            return "Miniature Thermal Emission Spectrometer (Mini-TES)"
+    // implemented getDataAsyncAwait for pagination...
+    /*
+    func getDataAsyncAwait(rover: String, date: String, camera: MarsCamera, page: Int) async throws -> [Photo] {
+        guard let url = createURLComponents(rover: rover, date: date, camera: camera, page: page).url else {
+            print("DEBUG: Bad URL")
+            throw URLError(.badURL)
+        }
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url)
+            
+            guard let response = response as? HTTPURLResponse else {
+                throw URLError(.badServerResponse)
+            }
+            guard response.statusCode >= 200 && response.statusCode < 300 else {
+                throw URLError(.dataNotAllowed)
+            }
+            guard !data.isEmpty else {
+                throw URLError(.fileDoesNotExist)
+            }
+            do {
+                let photoResponse = try JSONDecoder().decode(PhotoResponseModel.self, from: data)
+                return photoResponse.photos
+            } catch {
+                print("DEBUG: Decoding error occurred: \(error.localizedDescription)")
+                throw error
+            }
+        } catch {
+            print("Failed to get data with async await: \(error)")
+            throw error
         }
     }
-}
+    */
